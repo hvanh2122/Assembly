9b95b4cb7f90702aa41a56426262c0005be58716
hvanh2122 <104127955+hvanh2122@users.noreply.github.com>
2022-07-10 09:20:44 -0700

    Add files via upload

---
+++ dem_snt.asm
@@ -0,0 +1,90 @@
+.data
+    tb db 'So cac so nguyen to trong day la: $'
+    arr db 1,-2,2,3,4,5,6,7,2,3,5,3,3,3,3,0
+    n db 16
+    du db ?
+    num db ?
+    so db 0
+    
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+    
+    mov ah, 9
+    lea dx, tb
+    int 21h
+    
+    lea si, arr
+    mov cl, n
+    
+    duyet:
+        mov al, [si]
+        mov num, al
+        
+        cmp num, 1
+        jng tiep
+        
+        mov bl, 1
+        
+        chia:
+            mov ax, 0
+            mov al, num
+            
+            inc bl
+            div bl
+            
+            mov du, ah
+            
+            cmp du, 0
+            jne chia
+            
+            cmp bl, num
+            je tinhtong
+            
+            jmp tiep
+            
+        tinhtong:
+            mov al, so
+            inc al
+            mov so, al
+            
+        tiep:
+            inc si
+            loop duyet
+            
+        mov al, so
+        mov cx, 0
+        mov bl, 10
+        
+        todec:  
+            mov ah, 0
+            mov dx, 0
+            div bl 
+            inc cx
+            
+            mov dl, ah
+            add dl, 30h
+            push dx
+            
+            cmp al, 0
+            je hienthi
+            
+            jmp todec
+            
+        hienthi:
+            pop dx
+            mov ah, 2
+            int 21h 
+            loop hienthi
+            
+        mov ah, 8
+        int 21h
+        
+        mov ah, 4ch
+        int 21h
+        
+        main endp
+end main
+            
+            
